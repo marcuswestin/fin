59b056653b8617299187340882e794ef6e38ff3f
Marcus Westin <marcus.westin@gmail.com>
2010-01-07 16:07:59 +0100

    the private method onItemViewClick starts with an underscore

--- js/browser/ItemReferenceView.js
+++ js/browser/ItemReferenceView.js
@@ -21,7 +21,7 @@ exports = Class(browser.UIComponent, function(supr){
 		// jsio doesn't support circular imports...
 		var itemView = new window.__gItemViewClass(this._itemReference, this._itemType, 'reference');
 		itemView.appendTo(this._element);
-		events.add(itemView.getElement(), 'click', bind(this, 'onItemViewClick'));
+		events.add(itemView.getElement(), 'click', bind(this, '_onItemViewClick'));
 	}
 	
 	this._onItemViewClick = function(e) {

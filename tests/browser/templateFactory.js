82709d75095e882136c2c16bc10282b840be35ad
Marcus Westin <marcus.westin@gmail.com>
2010-02-06 00:53:16 -0800

    update test case for milestone patch

--- tests/browser/templateFactory.js
+++ tests/browser/templateFactory.js
@@ -4,14 +4,14 @@ jsio('import browser.templateFactory as templateFactory');
 
 exports.testValueReplacement = function() {
 	var template = '<div>{{ name }}</div><div>{{ email }}</div>';
-	var replaced = templateFactory._replaceValuesWithWidgets(template);
+	var replaced = templateFactory._replaceValuesWithViews(template);
 	var shouldBe = '<div>(( _itemValueView name ))</div><div>(( _itemValueView email ))</div>';
 	assert.equal(replaced, shouldBe, replaced + '\n!=\n' + shouldBe);
 }
 
 exports.testReferenceReplacement = function() {
 	var template = '<div>{{ owner : user }}</div><div>{{milestone:milestone}}</div>';
-	var replaced = templateFactory._replaceReferencesWithWidgets(template);
+	var replaced = templateFactory._replaceReferencesWithViews(template);
 	var shouldBe = '<div>(( _itemReferenceView \'user\' owner ))</div>' +
 		'<div>(( _itemReferenceView \'milestone\' milestone ))</div>';
 	assert.equal(replaced, shouldBe, '\n' + replaced + '\n!=\n' + shouldBe);
@@ -19,7 +19,7 @@ exports.testReferenceReplacement = function() {
 
 exports.testListReplacement = function() {
 	var template = '<div>[[ users ]]</div><div>[[buttons]]</div>';
-	var replaced = templateFactory._replaceListsWithWidgets(template);
+	var replaced = templateFactory._replaceListsWithViews(template);
 	var shouldBe = '<div>(( _itemListView users ))</div>' +
 		'<div>(( _itemListView buttons ))</div>';
 	assert.equal(replaced, shouldBe, '\n' + replaced + '\n!=\n' + shouldBe);

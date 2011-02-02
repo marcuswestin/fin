58a5710400ce22e171c316387129019b0e59129e
Marcus Westin <marcus.westin@gmail.com>
2011-02-02 17:37:42 -0500

    Use latest version of browser-require, and put all the demo client js in demo_client.js

--- Makefile
+++ Makefile
@@ -21,7 +21,7 @@ lib/redis-node-client:
 lib/browser-require:
 	git clone git://github.com/marcuswestin/browser-require.git
 	mv browser-require lib/
-	cd lib/browser-require; git checkout 65a472e92c7f59a98a63
+	cd lib/browser-require; git checkout dc6f1cff1b98e3cf9d7b35a929aaaed64aca36f5
 
 lib/socket.io:
 	git clone git://github.com/LearnBoost/Socket.IO-node.git socket.io
---
+++ demo_client.js
@@ -0,0 +1,62 @@
+var fin = require('./fin')
+
+var demo = {}
+
+fin.connect(function(){ 
+	var createButton = document.getElementById('create-button'),
+		createPropInput = document.getElementById('create-prop-input'),
+		createValInput = document.getElementById('create-val-input'),
+		subscribeIdInput = document.getElementById('subscribe-itemId'),
+		subscribePropInput = document.getElementById('subscribe-property'),
+		subscribeButton = document.getElementById('subscribe-button')
+		
+	createButton.onclick = function() {
+		var properties = {},
+			propName = createPropInput.value,
+			propVal = createValInput.value
+			
+		properties[propName] = propVal
+		fin.create(properties, function(newItemId) {
+			var el = demo.createOutput()
+			function update(newVal) { el.innerHTML = '<strong>created</strong> item ' + newItemId}
+			update(propVal)
+			// fin.observe(newItemId, propName, function(mutation) { update(mutation.args[0]) })
+		})
+	}
+	
+	subscribeButton.onclick = function() {
+		var itemId = subscribeIdInput.value
+		var propName = subscribePropInput.value
+		demo.subscribeTo(itemId, propName)
+	}
+	
+	createPropInput.onkeyup = function() { 
+		createButton.innerHTML = 'fin.create({"'+createPropInput.value+'":"'+createValInput.value+'"})' 
+	}
+	createValInput.onkeyup = createPropInput.onkeyup
+	
+	subscribeIdInput.onkeyup = function() { 
+		subscribeButton.innerHTML = 'fin.observe('+subscribeIdInput.value+',"'+subscribePropInput.value+'")'
+	}
+	subscribePropInput.onkeyup = subscribeIdInput.onkeyup
+	
+	createValInput.onkeyup()
+	subscribeIdInput.onkeyup()
+})
+
+demo.createOutput = function() {
+	var parent = document.getElementById('output-column')
+	var el = parent.insertBefore(document.createElement('div'), parent.childNodes[2])
+	el.className = 'header'
+	return el
+}
+
+demo.subscribeTo = function(itemId, propName) {
+	itemId = parseInt(itemId)
+	var el = demo.createOutput()
+	el.innerHTML = '<strong>subscribed</strong> to item ' + itemId + ' ' + propName + ' = <input style="width: 100px;"></input>'
+	el.className += ' item'
+	var input = el.getElementsByTagName('input')[0]
+	fin.observe(itemId, propName, function(mutation, newVal) { input.value = newVal })
+	input.onkeyup = function() { fin.set(itemId, propName, input.value) }
+}

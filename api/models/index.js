4ebd5cd2f8de5fc018dcc68be55fd82558437f9c
Marcus Westin <marcus.westin@gmail.com>
2011-02-17 00:55:08 -0500

    Model validation works

--- api/models/index.js
+++ api/models/index.js
@@ -14,7 +14,17 @@ function process(modelDescriptions) {
 }
 
 var _validateModel = function(modelName, properties) {
-	
+	var firstLetterCode = modelName.charCodeAt(0)
+	assert(65 <= firstLetterCode && firstLetterCode <= 90, 'Model names should start with an upper case letter. "'+modelName+'" does not.')
+	assert(!models[modelName], 'Model "'+modelName+'" already exists')
+	var propertyIDs = {}
+	for (propertyName in properties) {
+		firstLetterCode = propertyName.charCodeAt(0)
+		var property = properties[propertyName]
+		assert(97 <= firstLetterCode && firstLetterCode <= 122, 'Property names should start with a lowercase letter. "'+propertyName+'" does not.')
+		assert(typeof property.id == 'number', 'Properties need an id. "'+propertyName+'" does not')
+		assert(!propertyIDs[property.id], 'Property IDs need to be unique. "'+modelName+'" has two properties with the id '+property.id+'')
+	}
 }
 
 var _createModel = function(modelName) {
@@ -23,4 +33,9 @@ var _createModel = function(modelName) {
 
 var _createModelProperties = function(modelName, properties) {
 	
+}
+
+function assert(isOK, msg) {
+	if (isOK) { return }
+	throw new Error(msg)
 }

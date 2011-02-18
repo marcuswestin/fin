8c1e8cff27b64ecedff788608d27ffcdc0b61a3a
Marcus Westin <marcus.westin@gmail.com>
2011-02-18 01:11:36 -0500

    Create a full fledged chat example. This does not work yet

--- api/models/index.js
+++ api/models/index.js
@@ -59,6 +59,21 @@ function PropertyModel(value) {
 	this._value = value
 }
 
+PropertyModel.prototype.observe = function(callback) {
+	var info = _getObservationInfo(this)
+	fin.observe(info.id, info.chain, function(mutation) {
+		callback(mutation.value, mutation.op)
+	})
+}
+
+var _getObservationInfo = function(propertyModel) {
+	var propertyNameChain = []
+	
+	while(propertyModel._parent) {
+		propertyNameChain.push(propertyModel._propertyID)
+		propertyModel = propertyModel._parent
+	}
+	return { id:propertyModel._id, chain:propertyNameChain }
 }
 
 /* TODO
--- demo/chat/client.js
+++ demo/chat/client.js
@@ -1,7 +1,9 @@
 // This is what we want - it doesn't work yet :)
 
-var fin = require('../../api/client'),
-	models = require('../../api/models')
+var models = require('../../api/models')
+
+var user = null,
+	$ = function(id) { return document.getElementById(id) }
 
 models.process({
 	"User": {
@@ -10,11 +12,26 @@ models.process({
 	},
 	"Message": {
 		"text": { id:1, type:"Text" },
-		"user": { id:2, type:"User" }
+		"from": { id:2, type:"User" }
+	},
+	"Global": {
+		"messages": { id:1, type:"List", of:"Message" }
 	}
 })
 
-var marcus = new models.User({ name:'Marcus Westin', age:25 }),
-	message = new models.Message({ user:marcus, text:'Hello, world' })
+$('login').onclick = function() {
+	user = new models.User({ name: $('username').value, age: 25 }).create()
+	$('username').onchange = function(value) { user.name.set(value) }
+	$('chat').style.display = 'block'
+}
 
-console.log(message)
+models.global.messages.on('add', function(message) {
+	var div = $('messages').appendChild(document.createElement('div')),
+		sender = div.appendChild(document.createElement('div')),
+		messageInput = div.appendChild(document.createElement('input'))
+	
+	message.from.name.observe(function(value) { sender.innerHTML = value + ':' })
+	
+	message.text.observe(function(value) { input.value = value })
+	input.onchange = function(value) { message.text.set(value) }
+})

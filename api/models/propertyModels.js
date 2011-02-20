1d31739b8e5190f7d77695f395db70aeea16a297
Marcus Westin <marcus.westin@gmail.com>
2011-02-19 20:57:23 -0500

    Start fleshing out the property models in earnest. We'll need .observe and .on to begin with, and correct behavior for Text/Number vs List/Set

--- api/models/propertyModels.js
+++ api/models/propertyModels.js
@@ -1,16 +1,41 @@
+var fin = require('../client')
+
 module.exports = {
 	"Text": PropertyModel,
-	"Number": PropertyModel
+	"Number": PropertyModel,
+	"List": PropertyModel
 }
 
-function PropertyModel(value) {
-	this._value = value
+/* Property model types (Text/Number, List/Set)
+ **********************************************/
+function PropertyModel(value) { this._value = value }
+PropertyModel.prototype = {
+	observe: _modelObserve,
+	on: _modelOn
 }
 
-PropertyModel.prototype.observe = function(callback) {
-	var info = _getObservationInfo(this)
+function _modelObserve(callback) {
+	_observe(this, function(mutation) {
+		callback(mutation.args, mutation.op)
+	})
+}
+
+function _modelOn(mutationType, callback) {
+	_observe(this, function(mutation) {
+		if (mutation.op != mutationType) { return }
+		// TODO if this is a List/Set model, and the type "of" is a CustomModel,
+		// then instantiate that model and pass it as the argument to callback
+		callback(mutation.args)
+	})
+}
+
+/* Util function. All callbacks get called
+ * in the context of the model passed in
+ *****************************************/
+var _observe = function(propertyModel, callback) {
+	var info = _getObservationInfo(propertyModel)
 	fin.observe(info.id, info.chain, function(mutation) {
-		callback(mutation.value, mutation.op)
+		callback.call(propertyModel, mutation)
 	})
 }
 

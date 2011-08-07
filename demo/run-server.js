296ae2a286ca9c584c9a1d40923137289192986d
Marcus Westin <marcus.westin@gmail.com>
2011-08-07 11:43:59 -0700

    Use latest require for serving the demo files in development

--- demo/run-server.js
+++ demo/run-server.js
@@ -1,15 +1,22 @@
 var http = require('http'),
 	fs = require('fs')
 	fin = require('../lib/server-api'),
-	engine = require('../engines/development')
+	engine = require('../engines/development'),
+	requireServer = require('require/server')
 
 var server = http.createServer(function(req, res) {
+	if (requireServer.isRequireRequest(req)) { return }
 	var demo = req.url.substr(1)
 	if (!demo) { listDemos(res) }
 	else { showDemo(demo, res) }
 })
 
 fin.mount(server, engine)
+
+requireServer
+	.addReplacement("'object' === typeof module ? module.exports : (window.io = {})", "window.io = {}")
+	.mount(server)
+	
 server.listen(1234)
 console.log('listening on :1234')
 
